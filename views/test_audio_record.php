12ee56375178406db18fe044ada1a0b514152cf3
Rogelio Sanchez <rnsanchez@trazalog.com>
2023-04-11 17:36:13 -0300

    Ejemplo funcional de grabación a 11025Hz con recorder.js

--- views/test_audio_record.php
+++ views/test_audio_record.php
@@ -1,63 +1,54 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Audio Recorder</title>
-</head>
-<body>
-	<h1>Audio Recorder</h1>
-	<button id="startRecording">Start Recording</button>
-	<button id="stopRecording" disabled>Stop Recording</button>
-	<button id="playRecording" disabled>Play Recording</button>
-	<audio id="player" src="" controls></audio>
 
-	<script>
-		var startRecordingButton = document.getElementById('startRecording');
-		var stopRecordingButton = document.getElementById('stopRecording');
-		var playRecordingButton = document.getElementById('playRecording');
-		var player = document.getElementById('player');
+  <h1>Recorder.js example</h1>
+  <button id="start-btn">Start Recording</button>
+  <button id="stop-btn">Stop Recording and Download</button>
+  <script>
+    var recorder = null;
+    var audioContext = null;
 
-		var mediaRecorder;
-		var chunks = [];
+    var startBtn = document.getElementById('btnGrabar');
+    var stopBtn = document.getElementById('btnDetener');
 
-		var handleDataAvailable = (event) => {
-			if (event.data.size > 0) {
-				chunks.push(event.data);
-			}
-		};
+    startBtn.onclick = function() {
+      if (recorder !== null) {
+        recorder.clear();
+        recorder.destroy();
+        recorder = null;
+      }
 
-		var startRecording = async () => {
-			try {
-				var stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-				var options = { mimeType: 'audio/webm;codecs=opus', audioBitsPerSecond: 11025 };
-				mediaRecorder = new MediaRecorder(stream, options);
-				mediaRecorder.ondataavailable = handleDataAvailable;
-				mediaRecorder.start();
-				startRecordingButton.disabled = true;
-				stopRecordingButton.disabled = false;
-				playRecordingButton.disabled = true;
-				player.src = '';
-			} catch (error) {
-				console.error('Error:', error);
-			}
-		};
+      if (audioContext !== null) {
+        audioContext.close();
+        audioContext = null;
+      }
 
-		var stopRecording = () => {
-			mediaRecorder.stop();
-			startRecordingButton.disabled = false;
-			stopRecordingButton.disabled = true;
-			playRecordingButton.disabled = false;
-		};
+      audioContext = new AudioContext({
+        sampleRate: 11025
+      });
 
-		var playRecording = () => {
-			var blob = new Blob(chunks, { type: 'audio/webm' });
-			var audioURL = window.URL.createObjectURL(blob);
-			player.src = audioURL;
-			player.play();
-		};
+      navigator.mediaDevices.getUserMedia({
+        audio: true
+      }).then(function(stream) {
+        var input = audioContext.createMediaStreamSource(stream);
+        recorder = new Recorder(input, {
+          numChannels: 1,
+          sampleRate: 11025
+        });
+        recorder.record();
+      }).catch(function(err) {
+        console.error('Error getting user media: ' + err);
+      });
+    };
 
-		startRecordingButton.addEventListener('click', startRecording);
-		stopRecordingButton.addEventListener('click', stopRecording);
-		playRecordingButton.addEventListener('click', playRecording);
-	</script>
-</body>
-</html>
+    stopBtn.onclick = function() {
+      if (recorder !== null) {
+        recorder.stop();
+        recorder.exportWAV(function(blob) {
+          var url = URL.createObjectURL(blob);
+          var link = document.createElement('a');
+          link.href = url;
+          link.download = 'recorded-audio.wav';
+          link.click();
+        });
+      }
+    };
+  </script>
